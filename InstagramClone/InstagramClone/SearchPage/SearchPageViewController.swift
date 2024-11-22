9c1253ca407013b9385dbd293ebf785883f5250e
Giorgi Beraia <77335712+KingFingolfin@users.noreply.github.com>
2024-11-22 22:46:59 +0400

    add page folders

---
+++ InstagramClone/InstagramClone/SearchPage/SearchPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchPageViewController.swift
+//  InstagramClone
+//
+//  Created by Giorgi on 22.11.24.
+//
+
+import UIKit
+
+class SearchPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
